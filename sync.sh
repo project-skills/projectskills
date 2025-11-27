47e751e48821689d6d6497ff46de44b8c28810fe
A KORNILOV <alex@MacBook-Air-A.local>
2025-11-27 11:43:36 +0300

    Обновление 2025-11-27 11:43

--- sync.sh
+++ sync.sh
@@ -9,7 +9,7 @@ git add .
 git commit -m "Обновление $(date '+%Y-%m-%d %H:%M')"
 
 if git push; then
-    osascript -e 'display notification "Сайт обновлён! Проверьте Actions." with title "✅ Project Skills"'
+    osascript -e 'display notification "Сайт обновлён! Проверьте Actions." with title "✅ Project Skills" sound name "Бриз"'
     open "https://github.com/project-skills/projectskills/actions"
 else
     osascript -e 'display notification "Ошибка git push!" with title "❌ Project Skills" sound name "Basso"'
