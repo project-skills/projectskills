5003d1f55fd94de73a9bff6196dd0e90b8c1afc5
A KORNILOV <alex@MacBook-Air-A.local>
2025-12-11 01:29:14 +0300

    Обновление 2025-12-11 01:29

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Навыки проектной деятельности",
+    pageTitle: "ProjectSkills",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
